2078efaaba6b8cfae4f7967791cc1d421879909c
Tosin Farai <tosinfarai@gmail.com>
2021-01-16 00:25:50 +0100

    initial commit

--- mycli.py
+++ mycli.py
@@ -1,7 +1,6 @@
 import argparse
-import os
-import sys
 import platform
+import os
 
 class Installer(argparse.Action):
     def __init__(self, option_strings, dest, nargs=None, **kwargs):
@@ -10,20 +9,41 @@ class Installer(argparse.Action):
         super (Installer, self).__init__(option_strings, dest, **kwargs)
 
     def __call__(self, parser, namespace, values, option_string=None):
-        if (platform.system() == 'Linux'):
-            if (values == 'nodejs'):
-                print('install nodejs')
-            elif (values == 'wget'):
-                print ('install wget')
-            elif (values == 'curl'):
-                print('install curl')
+        if (values == 'nodejs'):
+            if (platform.system() == 'Linux'):
+                os.system('sudo apt-get install nodejs')
+            elif (platform.system() == 'Windows'):
+                os.system('scoop install nodejs')
+            elif (platform.system() == 'Darwin'):
+                os.system('brew install node')
             else:
-                raise ValueError("try again")
+                print('unrecognized OS')
+
+        elif (values == 'wget'):
+            if (platform.system() == 'Linux'):
+                os.system('sudo apt-get install wget')
+            elif (platform.system() == 'Windows'):
+                os.system('scoop install wget')
+            elif (platform.system() == 'Darwin'):
+                os.system('brew install wget')
+            else:
+                print('unrecognized OS')
+
+        elif (values == 'curl'):
+            if (platform.system() == 'Linux'):
+                os.system('sudo apt-get install curl')
+            elif (platform.system() == 'Windows'):
+                os.system('scoop install curl')
+            elif (platform.system() == 'Darwin'):
+                os.system('brew install curl')
+            else:
+                print('unrecognized OS')
+
+        else:
+            print("Error: Argument Unknown. Try 'nodejs', 'wget' or 'curl'")
 
 
-parser.add_argument('nodejs', action=Installer, type=str)
-parser.add_argument('wget', action=Installer, type=str)
-parser.add_argument('curl', action=Installer, type=str)
-
+parser = argparse.ArgumentParser()
+parser.add_argument('--install', action=Installer, type=str)
 
 args = parser.parse_args()
